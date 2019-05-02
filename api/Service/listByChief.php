0e1ea3f016f0c88c52cfc0c8d1cbe2ea836ba7b6
jbertho <james.bertho94@gmail.com>
2019-05-02 13:41:46 +0200

    serviceChiefFix

--- api/Service/listByChief.php
+++ api/Service/listByChief.php
@@ -7,7 +7,7 @@
  */
 
 header('Content-Type: application/json');
-require_once __DIR__.'/../../services/UserService.php';
+require_once __DIR__.'/../../services/ServiceService.php';
 require_once __DIR__.'/../../utils/validator.php';
 
 $body = file_get_contents('php://input');//lecture du body en chaine de car
@@ -16,10 +16,11 @@ $json = json_decode($body,true); //true -> decode as array
 print_r($json);
 if (isset($_GET['id'])){
 
-    $user = UserService::getInstance()->getById($_GET['id']);
-    if ($user){
+    $service = ServiceService::getInstance()->allServiceByChief($_GET['id']);
+
+    if ($service){
         http_response_code(201);
-        echo json_encode($user);
+        echo json_encode($service);
     }else{
         http_response_code(500);
     }
