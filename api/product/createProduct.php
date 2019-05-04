efd166a0790a5acb5ad74e053b729fb8a8878272
jbertho <james.bertho94@gmail.com>
2019-05-04 13:47:32 +0200

    testupload

--- api/product/createProduct.php
+++ api/product/createProduct.php
@@ -10,9 +10,11 @@ header('Content-Type: application/json');
 require_once __DIR__.'/../../services/ProductService.php';
 require_once __DIR__.'/../../utils/validator.php';
 
-if (isset($_GET['name']) and isset($_GET['price']) and isset($_GET['description']) and isset($_GET['pict']) and isset($_GET['service'])  ){
+if (isset($_POST['name']) and isset($_POST['price']) and isset($_POST['description']) and isset($_POST['pict']) and isset($_POST['service'])  ){
 
-    $product = ProductService::getInstance()->createProduct($_GET['name'],$_GET['price'],$_GET['description'],$_GET['pict'],$_GET['service']);
+    $image = $_POST['pict'];
+    file_put_contents("images/"+$_POST['service']+"/test.jpeg",base64_decode($image));
+    $product = ProductService::getInstance()->createProduct($_POST['name'],$_POST['price'],$_POST['description'],"images/"+$_POST['service']+"/test.png",$_POST['service']);
 
     if ($product == true){
         http_response_code(201);
