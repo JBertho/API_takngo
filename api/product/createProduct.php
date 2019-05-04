66dc0ad9ee7c8bcf9296cf3d407a8535561f384c
jbertho <james.bertho94@gmail.com>
2019-05-04 15:04:21 +0200

    testupload

--- api/product/createProduct.php
+++ api/product/createProduct.php
@@ -13,9 +13,6 @@ require_once __DIR__.'/../../utils/validator.php';
 if (isset($_POST['name']) and isset($_POST['price']) and isset($_POST['description']) and isset($_POST['pict']) and isset($_POST['service'])  ){
 
     $img = $_POST['pict'];
-    echo json_encode($img);
-    $img = str_replace('data:image/png;base64,', '', $img);
-    $img = str_replace(' ', '+', $img);
     $data = base64_decode($img);
     $file = uniqid() . '.png';
     $success = file_put_contents($file, $data);
