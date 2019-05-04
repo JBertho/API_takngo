10562a448f93a270af45f02959be6d545d05024f
jbertho <james.bertho94@gmail.com>
2019-05-04 23:31:55 +0200

    updatemodif

--- api/product/updateProduct.php
+++ api/product/updateProduct.php
@@ -13,6 +13,7 @@ require_once __DIR__.'/../../utils/validator.php';
 if (isset($_POST['id']) and isset($_POST['name']) and isset($_POST['price']) and isset($_POST['description']) and isset($_POST['pict']) and isset($_POST['modif'])  ){
 
     $img = $_POST['pict'];
+    $path= "non";
 
     if ($_POST['modif'] == 1) {
         $data = base64_decode($img);
@@ -26,9 +27,10 @@ if (isset($_POST['id']) and isset($_POST['name']) and isset($_POST['price']) and
         $name = uniqid() . '.png';
         $file = "../../../takngo/public/images/product/" . $_POST['service'] . "/" . $name;
         $success = file_put_contents($file, $data);
+        $path = "../../images/product/".$_POST['service']."/".$name;
     }
 
-    $product = ProductService::getInstance()->updateProduct($_POST['id'],$_POST['name'],$_POST['price'],$_POST['description'],$_POST['pict'],$_POST['modif']);
+    $product = ProductService::getInstance()->updateProduct($_POST['id'],$_POST['name'],$_POST['price'],$_POST['description'],$path,$_POST['modif']);
 
     if ($product == true){
         http_response_code(201);
