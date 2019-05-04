f91958bc6f42920275a9a0e124af5a7425e3b73b
jbertho <james.bertho94@gmail.com>
2019-05-04 14:05:26 +0200

    testupload

--- api/product/createProduct.php
+++ api/product/createProduct.php
@@ -13,7 +13,7 @@ require_once __DIR__.'/../../utils/validator.php';
 if (isset($_POST['name']) and isset($_POST['price']) and isset($_POST['description']) and isset($_POST['pict']) and isset($_POST['service'])  ){
 
     $image = $_POST['pict'];
-    file_put_contents("images/".$_POST['service']."/test.jpeg",base64_decode($image));
+    file_put_contents("test.jpg",base64_decode($image));
     $product = ProductService::getInstance()->createProduct($_POST['name'],$_POST['price'],$_POST['description'],"images/".$_POST['service']."/test.png",$_POST['service']);
 
     if ($product == true){
